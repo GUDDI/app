116eb0d24d12779cd4b12d8a60e2ce095efd8f0b
Clovis Junior <clovisjunior2009@gmail.com>
2013-08-09 15:11:36 -0300

    Criação das propriedades gerais do sistema com a remoção da classe de configuração de e-mail

--- src/main/java/br/org/guddi/business/SecurityBC.java
+++ src/main/java/br/org/guddi/business/SecurityBC.java
@@ -14,7 +14,7 @@ import br.gov.frameworkdemoiselle.lifecycle.Startup;
 import br.gov.frameworkdemoiselle.mail.Mail;
 import br.gov.frameworkdemoiselle.mail.internal.Config;
 import br.gov.frameworkdemoiselle.stereotype.BusinessController;
-import br.org.guddi.constant.MailConfig;
+import br.org.guddi.constant.GuddiConfig;
 import br.org.guddi.domain.Recurso;
 import br.org.guddi.domain.Usuario;
 import br.org.guddi.persistence.RecursoDAO;
@@ -31,12 +31,15 @@ public class SecurityBC {
 
     @Inject
     private Mail mailer;
+    
     @Inject
     private RecursoDAO recursoDAO;
+    
     @Inject
     private UsuarioDAO usuarioDAO;
+    
     @Inject
-    private MailConfig mailConfig;
+    private GuddiConfig guddiConfig;
     
     @Inject
     private Config config;
@@ -92,12 +95,12 @@ public class SecurityBC {
                 .append("\n\r Usuário: ").append(usu.getUsuario())
                 .append("\n\r Senha: ").append(usu.getSenha())
                 .append("\n\r siga o link para alterar sua senha: ")
-                .append(mailConfig.getUrl()).append(usu.getAminesia());
+                .append(guddiConfig.getUrl()).append(usu.getAminesia());
 
         mailer.to(destinatario)
-                .from(mailConfig.getRemetente())
+                .from(guddiConfig.getRemetente())
                 .body().text(texto.toString())
-                .subject(mailConfig.getAssunto())
+                .subject(guddiConfig.getAssunto())
                 .send();
         
     }
